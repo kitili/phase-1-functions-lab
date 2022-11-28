a52e16554c126580b5fdc2d998c21fab005e441e
kitili <you@example.com>
2022-11-28 05:33:21 -0800

    -msdfghj

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlocks) {
+    if (distanceInBlocks > 42) {
+      return distanceInBlocks - 42;
+    } else {
+      return 42 - distanceInBlocks;
+    }
+  }
+  function distanceFromHqInFeet(distanceInBlocks) {
+    return distanceFromHqInBlocks(distanceInBlocks) * 264;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    return destination > start
+      ? (destination - start) * 264
+      : (start - destination) * 264;
+  }
+  function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+      return 0;
+    } else if (
+      (400 < distanceTravelledInFeet(start, destination),
+      distanceTravelledInFeet(start, destination) < 2000)
+    ) {
+      return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    } else if (
+      (2000 < distanceTravelledInFeet(start, destination),
+      distanceTravelledInFeet(start, destination) < 2500)
+    ) {
+      return 25;
+    } else if (2500 < distanceTravelledInFeet(start, destination)) {
+      return `cannot travel that far`;
+    }
+  }
